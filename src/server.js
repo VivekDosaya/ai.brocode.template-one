6ff45f47f1f52df864a593badedd6c63307a8907
VivekDosaya <53901718+VivekDosaya@users.noreply.github.com>
2023-03-31 20:59:34 +0530

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const ejs = require("ejs");
+const fs = require("fs");
+
+const app = express();
+const port = 3000;
+
+// Set the view engine to EJS
+app.set("view engine", "ejs");
+
+// Define the route for the /template endpoint
+app.get("/template", (req, res) => {
+  // Read the EJS file
+  const template = fs.readFileSync("../views/index.ejs", "utf-8");
+
+  // Define the data to be used in the template
+  const data = {
+    title: "My Website",
+    body: "Welcome to my website!",
+  };
+
+  // Render the EJS file with the provided data as context and send it as the response
+  res.render("example", data);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
